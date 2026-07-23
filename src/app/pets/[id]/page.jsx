5fdf139ea8ee81842186a1817db00791c8f6b6e7
WahiduzzamanSakib <wahidzamanpg@gmail.com>
2026-07-23 09:23:12 +0600

    update

--- src/app/pets/[id]/page.jsx
+++ src/app/pets/[id]/page.jsx
@@ -13,6 +13,7 @@ const {token} = await auth.api.getToken({
   headers : await headers()
 })
 
+// console.log(token)
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/pets/${id}`,{
     headers: {
